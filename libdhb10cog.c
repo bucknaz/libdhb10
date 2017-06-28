90ca04d676154946c009920a358a565d8320ad87
bucknaz <buck@azmule.com>
2017-06-28 05:55:18 -0700

    added more locking

--- libdhb10cog.c
+++ libdhb10cog.c
@@ -62,48 +62,66 @@ static volatile int dbh10_cog;               // Global var for cogs to share
 static unsigned int dbh10_stack[40 + 25];    // Stack vars for other cog
 
 
-//Command to send
-static volatile int ready = 0;
-static volatile char cmd[10];
+//Command and reply buffers 
+char dhb10_reply[DHB10_LEN];//only used inside the cog
+char dhb10_cmd[DHB10_LEN]; //used to pass commands to the cog (locking)
 
-//Values returned 
+//Flag to indicate command ready
+static volatile int cmd_ready=0;
+
+//A place to store Values returned by cog 
 static volatile int heading = 0;
 static volatile int left_spd = 0;
 static volatile int right_spd = 0;
 static volatile int left_dist = 0;
 static volatile int right_dist = 0;
 
-//fdserial port varriables
-char dhb10_reply[DHB10_LEN];
-char dhb10_cmd[DHB10_LEN];
-
-
 
 
 //Functions for interacting with the cog
 int get_heading()
 {
-  return(heading);  
+  int V;
+  while (lockset(lockId) != 0) { /*spin lock*/ }
+  V = heading;
+  lockclr(lockId);  
+  return(V);
 }  
 
 int get_left_speed()
 {
-  return(left_spd);
+  int V;
+  while (lockset(lockId) != 0) { /*spin lock*/ }
+  V = left_spd;
+  lockclr(lockId);
+  return(V);
 }  
 
 int get_right_speed()
 {
-  return(right_spd);  
+  int V;
+  while (lockset(lockId) != 0) { /*spin lock*/ }
+  V = right_spd;
+  lockclr(lockId);  
+  return(V);
 }
   
 int get_left_distance()
 {
-  return(left_dist);  
+  int V;
+  while (lockset(lockId) != 0) { /*spin lock*/ }
+  V = left_dist;
+  lockclr(lockId);  
+  return(V);
 }
   
 int get_right_distance()
 {
-  return(right_dist);  
+  int V;
+  while (lockset(lockId) != 0) { /*spin lock*/ }
+  V = right_dist;
+  lockclr(lockId);
+  return(V);
 }  
 
 
@@ -111,6 +129,7 @@ void dhb10_gospd(int l, int r)
 {
   while (lockset(lockId) != 0) { /*spin lock*/ }
   sprint(dhb10_cmd, "gospd %d %d\r",l,r);
+  cmd_ready = 1;     
   lockclr(lockId);
 }  
 
@@ -121,26 +140,35 @@ void dhb10_gospd(int l, int r)
 
 
 /*
- *
- *
+ *  dbh10_start()
+ *  Start the cog running
+ *  uses fdserial so 2 cogs are consumed
  */
 int dbh10_start(void)
 {
   lockId = locknew(); // get a new lock
   lockclr(lockId); //Don't know if this is needed or not
   dbh10_stop();
+  cmd_ready = 0;//Just starting no commands to send
   dbh10_cog = 1 + cogstart(dhb10_comunicator, NULL, dbh10_stack, sizeof(dbh10_stack));
   return(dbh10_cog);
 }
 
 /*
- *
- *
+ *  dbh10_stop()
+ *  shut the cog off and free resources
  */
 void dbh10_stop(void)
 {
   if(dbh10_cog)
   {
+    while(cmd_ready != 99)
+    { 
+      while (lockset(lockId) != 0) { /*spin lock*/ }  
+      cmd_ready = 98;//flag cog to close serial port
+      lockclr(lockId);
+      pause(100);//Dont use 100% cpu clocks
+    }         
     cogstop(dbh10_cog -1);
     dbh10_cog = 0;
     lockclr(lockId); //Don't know if this is needed or not can't hurt
@@ -156,23 +184,29 @@ void dbh10_stop(void)
 //This is the cog that we run
 void dhb10_comunicator(void *par)
 {
- int state = 0; 
- int nocommand = 1;
+ int state = 0; //For the state mach
+ int local_cmd_ready;
  //Clear the comand and replay buffers
  memset(dhb10_reply, 0, DHB10_LEN);
  memset(dhb10_cmd, 0, DHB10_LEN);
  _dhb10_open(); 
+ 
  while(1)
  {
   // We will continuosly poll the board for for 3 value sets when 
   // we are just spinning waiting for somthing to do   
 
-   if(nocommand)
+   //Fetch a local copy of cmd_ready
+   while (lockset(lockId) != 0) { /*spin lock*/ } 
+   local_cmd_ready = cmd_ready;
+   lockclr(lockId);
+   
+   if(!local_cmd_ready)
    {
      switch(state)
      {
        case 0:
-        _dhb10_speed();
+        _dhb10_speed();        
         memset(dhb10_reply, 0, DHB10_LEN);//clear the reply string
         _dhb10_recive(dhb10_reply);
         if(dhb10_reply[0] == 'E')
@@ -180,9 +214,11 @@ void dhb10_comunicator(void *par)
           ;
         }
         else
-        {          
+        { 
           state++;         
+          while (lockset(lockId) != 0) { /*spin lock*/ }         
           sscan(dhb10_reply, "%d%d", &left_spd, &right_spd);
+          lockclr(lockId);
         }        
         break;
 
@@ -195,9 +231,11 @@ void dhb10_comunicator(void *par)
           ;
         }
         else
-        {          
+        { 
           state++; 
+          while (lockset(lockId) != 0) { /*spin lock*/ }         
           sscan(dhb10_reply, "%d", &heading);
+          lockclr(lockId);
         }        
         break;
 
@@ -210,28 +248,50 @@ void dhb10_comunicator(void *par)
          ; 
         }
         else
-        {          
+        { 
           state = 0; 
+          while (lockset(lockId) != 0) { /*spin lock*/ }         
           sscan(dhb10_reply, "%d%d", &left_dist, &right_dist);
+          lockclr(lockId);
         }          
         break;
         
        default:
         state = 0; 
         break;
+        
      }//End switch      
-    }//End if(nocommand)       
+    }//End if(!cmd_ready) 
+    else if(local_cmd_ready == 98)//Close the terminal for orderly shutdown
+    {
+      _dhb10_close();
+      while (lockset(lockId) != 0) { /*spin lock*/ }  
+      cmd_ready = 99; //to indicate we are done
+      lockclr(lockId);
+      while(1){pause(1000);}//spin here forever
+    }           
     else
     {
       //send command and fetch results
+      while (lockset(lockId) != 0) { /*spin lock*/ }
+      _dhb10_cmd(dhb10_cmd);
+      cmd_ready = 0; 
+      lockclr(lockId);//Clear the lock and allow others to setup command
       
-    }
+      memset(dhb10_reply, 0, DHB10_LEN);//clear the reply string
+      _dhb10_recive(dhb10_reply);
+      if(dhb10_reply[0] == 'E')
+      {
+       ; 
+      }
+      else
+      {   
+            
+      }          
+      
+    }//End if(!cmd_ready) else
           
    pause(100); 
 
  }//End While    
-
- _dhb10_close();
- cogstop(dbh10_cog -1);
- dbh10_cog = 0;
 }  
